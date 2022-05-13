d9d205bbd3186398ac1b44f594881f726f093075
Chiriboga Daniel <danychirizurita@hotmail.com>
2022-05-12 21:12:16 -0500

    V1.0.0

--- workshops/chiribogad/unit1/WS02HelloWorld/HelloWorld.java
+++ workshops/chiribogad/unit1/WS02HelloWorld/HelloWorld.java
@@ -1,5 +1,5 @@
 class HelloWorld {
 	public static void main(String[] args){
-		System.out.println("Hello World ! from Elkin Pabon");
+		System.out.println("Hello World ! from Daniel Chiriboga");
 	}
 }
