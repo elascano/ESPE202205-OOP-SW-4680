7098bb12f202e5a55bdfed529740c45d364257aa
DanielaLTM2206 <danielatituaa@gmail.com>
2022-05-24 11:13:07 -0500

    WSO6

---
+++ workshops/tituañad/unit1/WS07FromUmlToCode/ChickenFarmer/src/chickenfarmer/ChickenFarmer.java
@@ -0,0 +1,15 @@
+package chickenfarmer;
+
+/**
+ ** @author Daniela Tituaña, DCCO-ESPE
+ */
+public class ChickenFarmer {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello Daniela´s Chickens");
+    }
+    
+}
