edd69610c1f803534f9fecd6609f3d6670289b07
Cesar Ignacio Loor Mercado <ciloor2@espe.edu.ec>
2022-05-28 19:24:22 -0500

    H05AntSimulatorCode

---
+++ assignments/loorc/unit1/HW05AntSimulatorCode/AntSimulator/src/ec/edu/espe/antsimulator/view/SimulatorDisplay.java
@@ -0,0 +1,26 @@
+package ec.edu.espe.antsimulator.view;
+
+import ec.edu.espe.antsimulator.model.Cell;
+import ec.edu.espe.antsimulator.model.PheromoneDrop;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Loor Cesar,DDCO-ESPE,GADC.MSI
+ */
+public class SimulatorDisplay {
+    public static void main(String[] args) {
+        System.out.println("This is the Ant Simulator");
+        
+        int xCoordinate;
+        
+        Cell cell;
+        ArrayList<PheromoneDrop> pheromoneDrops;
+        pheromoneDrops = new ArrayList<>();
+        cell = new Cell(3,4,pheromoneDrops);
+        
+        System.out.println("cell -->"+ cell);
+         
+        
+    }
+}
