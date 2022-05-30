50e2fa63b1997b848b77d20fa20f7fda702e032a
Ronny Ibarra <rjibarra1@espe.edu.ec>
2022-05-30 16:41:08 -0500

    WS08AntSimulator

--- workshops/ibarrar/unit1/WS08Association/AntSimulator/src/ec/edu/espe/antsimulator/model/FoodPile.java
+++ workshops/ibarrar/unit1/WS08Association/AntSimulator/src/ec/edu/espe/antsimulator/model/FoodPile.java
@@ -6,52 +6,13 @@ package ec.edu.espe.antsimulator.model;
  */
 public class FoodPile {
     
-    private int pileAmount;
-    private Cell position;
+    int pileAmount;
+    Cell position;
 
-    public FoodPile(int pileAmount, Cell position) {
-        this.pileAmount = pileAmount;
-        this.position = position;
-    }
-
-    @Override
-    public String toString() {
-        return "FoodPile{" + "pileAmount=" + pileAmount + ", position=" + position + '}';
-    }
-    
-    
-    
-    public Food yliedFood( ){
-        return null;
-        
-    }
-
-    /**
-     * @return the pileAmount
-     */
-    public int getPileAmount() {
-        return pileAmount;
-    }
-
-    /**
-     * @param pileAmount the pileAmount to set
-     */
-    public void setPileAmount(int pileAmount) {
-        this.pileAmount = pileAmount;
-    }
-
-    /**
-     * @return the position
-     */
-    public Cell getPosition() {
-        return position;
-    }
-
-    /**
-     * @param position the position to set
-     */
-    public void setPosition(Cell position) {
-        this.position = position;
+    public Food yielfFood(int amount){
+        Food food;
+        food = new Food(amount);
+        return food;
     }
     
 }
