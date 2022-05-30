0f4ffc28899c0e0008b85057f50f531d9a11b3f0
rabedon1 <alexanderbedon58724222@gmail.com>
2022-05-30 13:52:12 -0500

    WS08Collection

--- workshops/bedonr/unit1/WS08Association/AntSimulator/src/ec/edu/espe/antsimulator/model/PheromoneDrop.java
+++ workshops/bedonr/unit1/WS08Association/AntSimulator/src/ec/edu/espe/antsimulator/model/PheromoneDrop.java
@@ -1,7 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package ec.edu.espe.antsimulator.model;
 
 /**
@@ -9,5 +6,29 @@ package ec.edu.espe.antsimulator.model;
  * @author Roberto Bedon, DCCO-ESPE,BettaCoders
  */
 public class PheromoneDrop {
-    
+    private int currentLevel;
+
+ public void run(){
+     System.out.println("Running with PheromoneDrop");
+ }
+
+    public PheromoneDrop(int currentLevel) {
+        this.currentLevel = currentLevel;
+    }
+
+    /**
+     * @return the currentLevel
+     */
+    public int getCurrentLevel() {
+        return currentLevel;
+    }
+
+    /**
+     * @param currentLevel the currentLevel to set
+     */
+    public void setCurrentLevel(int currentLevel) {
+        this.currentLevel = currentLevel;
+    }
+ 
+ 
 }
