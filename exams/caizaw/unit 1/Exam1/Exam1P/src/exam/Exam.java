e0ad555e32f41d1d7a289450ababb4be2ae978c2
Dastrum <93009937+Dastrum@users.noreply.github.com>
2022-06-01 09:10:16 -0500

    examen1P

---
+++ exams/caizaw/unit 1/Exam1/Exam1P/src/exam/Exam.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package exam;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Widinson Caiza, DCCO- ESPE, BettaCoders
+ */
+public class Exam {
+
+    public static void main(String[] args) {
+        ArrayList<Book> books;
+        int sizeBooks;
+        books = new ArrayList<>(); 
+        
+        
+        for(int i=0; i<9;i++){
+            books.add(i, new Book("title1", "Autho1", "Editorial1", "2022"));
+        }
+        sizeBooks= books.size();
+        for(Book printf: books){
+            System.out.println(printf);
+        }
+        System.out.println("Quantity of books: "+ sizeBooks);
+    }
+    
+}
