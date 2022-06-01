3e2afe94912cd5c0cd22b62ba97bd7ef8b9a7986
Ronny Ibarra <rjibarra1@espe.edu.ec>
2022-06-01 09:20:13 -0500

    POO EXAM

---
+++ exams/ibarrar/unit1/ExamPoo/src/ec/edu/espe/ExamPoo/view/SimulatroDisplay.java
@@ -0,0 +1,23 @@
+package ec.edu.espe.ExamPoo.view;
+
+import ec.edu.espe.ExamPoo.model.Book;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Ronny Ibarra, DCCO-ESPE, Syntax Error
+ */
+public class SimulatroDisplay {
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        ArrayList<Book> books;
+        books = new ArrayList<>();
+        
+        books.add(new Book("Algebra","Stanley","Baldor",2018));
+    }
+    
+}
+
