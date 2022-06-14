1f43be8faf5f20ee6f16c4d58ebdaac6f6fe2fa6
RonnyIbarra <rjibarra1@espe.edu.ec>
2022-06-14 09:36:57 -0500

    Primitive Arrays

---
+++ workshops/ibarrar/unit2/WS14 Arrays/PrimitiveArrays/src/ec/edu/espe/primitive/view/University.java
@@ -0,0 +1,29 @@
+
+package ec.edu.espe.primitive.view;
+
+
+
+/**
+ *
+ * @author Ronny Ibarra,DDCO-ESPE,Syntax Error
+ */
+public class University {
+
+      public static void main(String[] args) {
+       
+
+        int numbers[];
+        numbers = new int[10];
+       
+        for(int i=0;i<5;i++){
+            
+            numbers[i]=i+1;
+        }
+        for (int i=0;i<10;i++){
+            System.out.println(numbers[i]);
+        
+        }
+          
+         
+    }
+}
