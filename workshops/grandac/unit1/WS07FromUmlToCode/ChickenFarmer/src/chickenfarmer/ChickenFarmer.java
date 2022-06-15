90f3e930e429cfe220b050860c6c814cecb91148
ErickMoreiraVinueza <epmoreira2@espe.edu.ec>
2022-06-15 16:10:05 -0500

    Refresh2

---
+++ workshops/grandac/unit1/WS07FromUmlToCode/ChickenFarmer/src/chickenfarmer/ChickenFarmer.java
@@ -0,0 +1,17 @@
+
+package chickenfarmer;
+
+/**
+ *
+ * @author Carlos Granda, Syntax Error-DCCO-ESPE
+ */
+public class ChickenFarmer {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello Carlos Chickens");
+    }
+    
+}
---
+++ workshops/montufard/unit1/WS07FromUmlToCode/ChickenFarmer/src/chickenfarmer/ChickenFarmer.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package antsimulator;
+
+/**
+ *
+ * @author David Montufar, DCCO-ESPE
+ */
+public class AntSimulator {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello David's Chicken's");
+    }
+    
+}
