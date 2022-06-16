f5b6055bc0b4fb2f2baa5c132bd99d4025fb5ea2
Martin Toapanta <mhtoapanta1@espe.edu.ec>
2022-06-16 09:05:56 -0500

    WS14 Array

---
+++ workshops/toapantam/unit2/WS15 Files/FileManager/src/FileManager.java
@@ -0,0 +1,17 @@
+/**
+ *
+ * @author MARTIN
+ */
+public class FileManager {
+    
+    public static String readFile(String fileName){
+       //code to read from fil
+       String data = "{\"name\":\"Edison\"}";
+       return data;
+    }
+    public static int add(int addend1, int addend2){
+        int sum;
+        sum = addend1+addend2;
+        return sum;
+    }
+}
--- workshops/toapantam/unit2/WS16 FileManager/JavaApplication10/src/ec/edu/espe/University/view/University.java
+++ workshops/toapantam/unit2/WS16 FileManager/JavaApplication10/src/ec/edu/espe/University/view/University.java
@@ -1,6 +1,14 @@
 package ec.edu.espe.University.view;
 
+import com.google.gson.Gson;
 import ec.edu.espe.utils.FileManager;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 
 
@@ -8,12 +16,22 @@ import ec.edu.espe.utils.FileManager;
 
 /**
  *
- * @author Martín Toapanta, DCCO-ESPE, MyWayCode 
+ * @author Martín Toapanta, DCCO-ESPE, MyWayCode
  */
 public class University {
     public static void main(String[] args) {
       //  FileManager fileManager=new FileManager();
         System.out.println("data recoverd from File Manager -->"+FileManager.readFile("File Manager"));
+        String pathJSON;
+        String dataJSON;
+        
+        
+        
+        
+        pathJSON = System.getProperty("user.dir")+ "\\data.json";
+        dataJSON=FileManager.readJson(pathJSON);
+        System.out.println("FUNCIONAAAA");
+        System.out.println(dataJSON);
         
         int addend1=5;
         int addend2=4;
@@ -21,4 +39,7 @@ public class University {
         sum=FileManager.add(addend1,addend2);
         System.out.println("the addition is -->"+sum);
     }
+    
+
 }
+
