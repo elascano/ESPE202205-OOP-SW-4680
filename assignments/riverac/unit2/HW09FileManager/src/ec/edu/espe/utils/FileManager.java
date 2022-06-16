a970034b1c9d1e293dab225aa8a8f9cc12f85c31
unknown <csrivera3@espe.edu.ec>
2022-06-16 07:43:02 -0500

    HS09

--- assignments/riverac/unit2/HW09FileManager/src/ec/edu/espe/utils/FileManager.java
+++ assignments/riverac/unit2/HW09FileManager/src/ec/edu/espe/utils/FileManager.java
@@ -1,20 +1,56 @@
 package ec.edu.espe.utils;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.util.Scanner;
+
 /**
  *
  * @author Carlos Rivera, DCCO-ESPE, CODEX++
  */
 public class FileManager {
-    
-public String readFile(String fileName){
-    
-        String data = "{\"name\":\"Carlos\"}";
-        return data;
-       }
 
-public int add(int addend1, int addend2){
-    int sum;
-    sum = addend1+addend2;
-    return sum;
-}
+    public static String readFile(String fileName) {
+        // code to read from file
+
+        String data = "From" + fileName + "-->{\"name\":\"Carlos\"}";
+        return data;
+    }
+
+    public static int add(int addend1, int addend2) {
+        int sum;
+        sum = addend1 + addend2;
+        return sum;
+    }
+
+    public static String readJson(String pathJSON) {
+
+        File file = new File(pathJSON);
+        Scanner readerFile;
+        String data;
+        data = "";
+
+        try {
+            readerFile = new Scanner(file);
+            FileWriter fileWriter = new FileWriter(file);
+            BufferedWriter bw = new BufferedWriter(fileWriter);
+            if (!(file.exists())) {
+                file.createNewFile();
+                fileWriter = new FileWriter(pathJSON);
+
+            } else {
+                fileWriter = new FileWriter(pathJSON, true);
+            }
+            bw.append(FileManager.readFile("data.json"));
+            bw.close();
+            data = readerFile.nextLine();
+
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+        return data;
+
+    }
+
 }
