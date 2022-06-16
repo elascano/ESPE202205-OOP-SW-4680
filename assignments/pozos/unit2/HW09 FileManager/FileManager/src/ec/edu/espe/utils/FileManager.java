97a9275ab1ae3c32d74ef5867ef4d3ebf6c826b9
stevenpozo <sjpozo1@espe.edu.ec>
2022-06-16 00:17:03 -0500

    Homework

---
+++ assignments/pozos/unit2/HW09 FileManager/FileManager/src/ec/edu/espe/utils/FileManager.java
@@ -0,0 +1,56 @@
+package ec.edu.espe.utils;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.util.Scanner;
+
+/**
+ *
+ * @author Steven Pozo, DCCO-ESPE, DEES Developers
+ */
+public class FileManager {
+
+    public static String readFile(String fileName) {
+        // code to read from file
+
+        String data = "From" + fileName + "-->{\"name\":\"Steven\"}";
+        return data;
+    }
+
+    public static int add(int addend1, int addend2) {
+        int sum;
+        sum = addend1 + addend2;
+        return sum;
+    }
+
+    public static String readJson(String pathJSON) {
+
+        File file = new File(pathJSON);
+        Scanner readerFile;
+        String data;
+        data = "";
+
+        try {
+            readerFile = new Scanner(file);
+            FileWriter fileWriter = new FileWriter(file);
+            BufferedWriter bw = new BufferedWriter(fileWriter);
+            if (!(file.exists())) {
+                file.createNewFile();
+                fileWriter = new FileWriter(pathJSON);
+
+            } else {
+                fileWriter = new FileWriter(pathJSON, true);
+            }
+            bw.append(FileManager.readFile("data.json"));
+            bw.close();
+            data = readerFile.nextLine();
+
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+        return data;
+
+    }
+
+}
