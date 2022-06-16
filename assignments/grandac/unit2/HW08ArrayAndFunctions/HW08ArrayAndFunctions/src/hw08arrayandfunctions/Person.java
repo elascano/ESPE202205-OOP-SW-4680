5826c8c435fc3196209479110c5a0acb4998e03f
CarlosJavierGrandaVelasquez <cjgranda1@espe.edu.ec>
2022-06-16 07:24:54 -0500

    HW08ArrayAndFunctions

---
+++ assignments/grandac/unit2/HW08ArrayAndFunctions/HW08ArrayAndFunctions/src/hw08arrayandfunctions/Person.java
@@ -0,0 +1,56 @@
+
+package hw08arrayandfunctions;
+
+/**
+ *
+ * @author Carlos Granda, DCCO- ESPE, Syntax Error
+ */
+public class Person {
+    private String name;
+    private int number;
+
+    public Person(String name, int number) {
+        this.name = name;
+        this.number = number;
+    }
+
+    @Override
+    public String toString() {
+        return "Person=" + "name=" + name + ", number=" + number ;
+    }
+    
+    
+    
+    
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * @return the number
+     */
+    public int getNumber() {
+        return number;
+    }
+
+    /**
+     * @param number the number to set
+     */
+    public void setNumber(int number) {
+        this.number = number;
+    }
+    
+    
+    
+}
