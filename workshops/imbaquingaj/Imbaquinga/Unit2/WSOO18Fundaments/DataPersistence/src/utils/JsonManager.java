17814db58bb5d40d6a63f3efcaaa767c89efc851
jrimbaquingaguana <ricardoimbaquinga@gmail.com>
2022-06-21 22:17:18 -0500

    WSOO18Fudaments

---
+++ workshops/imbaquingaj/Imbaquinga/Unit2/WSOO18Fundaments/DataPersistence/src/utils/JsonManager.java
@@ -0,0 +1,13 @@
+package utils;
+
+/**
+ *
+ * @author Jose Imbaquinga,DCCO-ESPE, DEES Syntaxi Error
+ */
+public class JsonManager extends FileManager {
+    @Override
+    public void insert(){
+        
+    }
+    
+}
