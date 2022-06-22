ae5d03ba1902dcfc3e5ef14d2aed71d11be6ae1c
CarlosJavierGrandaVelasquez <cjgranda1@espe.edu.ec>
2022-06-21 19:52:20 -0500

    WS18 DataPersistence

---
+++ workshops/grandac/unit2/WS18OOFundamentals/DataPersistence/src/Utils/JsonManager.java
@@ -0,0 +1,14 @@
+package Utils;
+
+/**
+ *
+ * @author Carlos Granda, DCCO-ESPE, Syntax Error
+ */
+public class JsonManager extends FileManager{
+
+    @Override
+    public void insert() {
+        
+    }
+    
+}
---
+++ workshops/grandac/unit2/WS18OOFundamentals/DataPersistence/src/Utils/MongoDBManager.java
@@ -0,0 +1,14 @@
+package Utils;
+
+/**
+ *
+ * @author Carlos Granda, DCCO-ESPE, Syntax Error
+ */
+public class MongoDBManager extends NoSqlManager{
+
+    @Override
+    public void insert() {
+        
+    }
+    
+}
