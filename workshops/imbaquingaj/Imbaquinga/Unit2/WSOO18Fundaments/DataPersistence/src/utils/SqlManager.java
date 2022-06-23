81a4836acb68621597b74eb2dade6a28ed6d344a
jrimbaquingaguana <ricardoimbaquinga@gmail.com>
2022-06-23 02:41:46 -0500

    WS18

--- workshops/imbaquingaj/Imbaquinga/Unit2/WSOO18Fundaments/DataPersistence/src/utils/SqlManager.java
+++ workshops/imbaquingaj/Imbaquinga/Unit2/WSOO18Fundaments/DataPersistence/src/utils/SqlManager.java
@@ -1,9 +1,10 @@
+
 package utils;
 
 /**
  *
- * @author Jose Imbaquinga,DCCO-ESPE, DEES Syntaxi Error
+ *author Jose Imbaquinga,Carlos Granda,DCCO-ESPE, DEES Syntaxi Erro
  */
 public abstract class SqlManager extends DBManager {
-    
+    String table;
 }
