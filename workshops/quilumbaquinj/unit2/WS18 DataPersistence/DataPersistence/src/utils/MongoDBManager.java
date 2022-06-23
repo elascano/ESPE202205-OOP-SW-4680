71fcfedc67ffdd96bc4fcc205d9db30b7019062a
Jairo Quilumbaquin <jsquilumbaquin@espe.edu.ec>
2022-06-22 20:45:23 -0500

    WS18

--- workshops/quilumbaquinj/unit2/WS18 DataPersistence/DataPersistence/src/utils/MongoDBManager.java
+++ workshops/quilumbaquinj/unit2/WS18 DataPersistence/DataPersistence/src/utils/MongoDBManager.java
@@ -2,12 +2,18 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package Utili;
+package utils;
 
 /**
  *
  * @author QUILUMBAQUIN JAIRO, DCC0-ESPE: CODEX++
  */
-public class JsonManager {
+public class MongoDBManager extends NoSqlManager {
+    String collection;
+
+    @Override
+    public void insert() {
+       
+    }
     
 }
