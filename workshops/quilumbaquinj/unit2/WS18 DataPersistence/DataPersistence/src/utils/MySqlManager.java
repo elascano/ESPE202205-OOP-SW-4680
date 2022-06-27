ddb2d46673b8f3bbac6b63e59360ee646a27c167
Jairo Quilumbaquin <jsquilumbaquin@espe.edu.ec>
2022-06-26 20:30:38 -0500

    HW13

--- workshops/quilumbaquinj/unit2/WS18 DataPersistence/DataPersistence/src/utils/MySqlManager.java
+++ workshops/quilumbaquinj/unit2/WS18 DataPersistence/DataPersistence/src/utils/MySqlManager.java
@@ -11,5 +11,29 @@ public class MySqlManager extends SqlManager {
     public void insert() {
         
     }
+
+    @Override
+    public void Create() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void Read() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void Update() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void Delete() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    public void EstablishConnection() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
     
 }
