7a6352ceae5e4d63ef9161ca1a7783b9e536f3eb
Edison Lascano <edison_lascano@yahoo.com>
2022-06-28 08:35:24 -0500

    interfaces

---
+++ workshops/elascano/unit2/NewFolder/FileManager/src/ec/edu/espe/zoo/model1/Mammal.java
@@ -0,0 +1,13 @@
+package ec.edu.espe.zoo.model1;
+
+/**
+ *
+ * @author Edison Lascano, DCCO-ESPE
+ */
+public abstract class Mammal extends Animal{
+    
+    public Mammal(int id, String description, String cage) {
+        super(id, description, cage);
+    }
+    
+}
