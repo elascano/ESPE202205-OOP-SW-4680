917a090618f8236fea3c1edb39da671da1b2bfbe
Luis Burbano <laburbano1@espe.edu.ec>
2022-06-27 19:12:16 -0500

    WS

---
+++ workshops/burbanol/unit2/WS16FileManager/PruebaMongoDb/src/pruebamongodb/Book.java
@@ -0,0 +1,71 @@
+
+package pruebamongodb;
+
+/**
+ *
+ * @author Luis Burbano, DCCO- ESPE, BettaCoders
+ */
+public class Book {
+    
+    private String title;
+    private String author;
+    private String publisher;
+
+    public Book(String title, String author, String publisher) {
+        this.title = title;
+        this.author = author;
+        this.publisher = publisher;
+    }
+
+    @Override
+    public String toString() {
+        return "Book --->" + "title=" + title + ", author=" + author + ", publisher=" + publisher;
+    }
+
+    
+    
+    /**
+     * @return the title
+     */
+    public String getTitle() {
+        return title;
+    }
+
+    /**
+     * @param title the title to set
+     */
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    /**
+     * @return the author
+     */
+    public String getAuthor() {
+        return author;
+    }
+
+    /**
+     * @param author the author to set
+     */
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    /**
+     * @return the publisher
+     */
+    public String getPublisher() {
+        return publisher;
+    }
+
+    /**
+     * @param publisher the publisher to set
+     */
+    public void setPublisher(String publisher) {
+        this.publisher = publisher;
+    }
+    
+    
+    
+}
