cdeef9c712eae4627fef449de15d752b1d97e91e
Chiriboga Daniel <danychirizurita@hotmail.com>
2022-06-29 22:39:59 -0500

    HW13 MongoDB_DataPersistence docx

---
+++ assignments/chiribogad/unit2/HW13 MongoDB_DataPersistence docx/MongoCrud/src/mongocrud/MongoCrud.java
@@ -0,0 +1,29 @@
+package mongocrud;
+
+import com.mongodb.client.MongoDatabase;
+
+/**
+ *
+ * @author Daniel Chiriboga, DCCO-ESPE, DAMAGE SE
+ */
+public class MongoCrud {
+
+    public static void main(String[] args) {
+        
+        Connection connection = new Connection();
+        MongoDatabase mongodb = connection.connectionDataBase();
+        TablesCrud tableCrud = new TablesCrud();
+        
+        Table table = new Table();
+        
+        table.setName("acacia");
+        table.setPrice(90);
+        
+        tableCrud.createTable(table);
+        tableCrud.readTable();
+        tableCrud.updateTable();
+        tableCrud.deleteTable();
+        tableCrud.readTable();
+    }
+    
+}
