41c26cc25e3ff8bcae1643f5ea86d6c617c54145
Chiriboga Daniel <danychirizurita@hotmail.com>
2022-06-29 22:42:33 -0500

    HW15FixCode

---
+++ assignments/chiribogad/unit2/HW15FixCode/ConditionalComplexity/src/ec/edu/espe/pitfalls/view/HW15Pitfalls.java
@@ -0,0 +1,47 @@
+package ec.edu.espe.pitfalls.view;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Daniel Chiriboga, DAMAGE SE-DCCO-ESPE
+ */
+public class HW15Pitfalls {
+
+    public static void main(String[] args) {
+        Scanner input;
+        int type;
+        
+        input= new Scanner(System.in);
+        
+        System.out.println("Seleccione el tipo de archivo que desea crear");
+        System.out.println("1.PDF\n");
+        System.out.println("2.DOC\n");
+        System.out.println("3.CSV\n");
+        System.out.println("4.TXT\n");
+        type=input.nextInt();
+        
+        
+        if(type==1){
+            System.out.println("PDF was created");
+        }else{
+            if(type==2){
+            System.out.println("DOC was created");
+        }else{
+                if(type==3){
+            System.out.println("CSV was created");
+        }else{
+        if(type==4){
+            System.out.println("TXT was created");
+        }else{
+        
+        }
+        }
+        
+        }
+        }
+        
+        
+    }
+    
+}
