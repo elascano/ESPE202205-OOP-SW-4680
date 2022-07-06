d7b6123d370ba1032edebda3bac7e924a16dc9dc
Widinson Danilo <widinsondanilo@gmail.com>
2022-07-05 22:47:50 -0500

    v1.0

--- assignments/caizaw/unit2/HW15longMessageChains/src/longmessagechains/LongMessageChains.java
+++ assignments/caizaw/unit2/HW15longMessageChains/src/longmessagechains/LongMessageChains.java
@@ -14,14 +14,14 @@ public class LongMessageChains {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        obj ob = new obj();
-        int id = obj.getDept().getSubDept().getHOD().getid();
-//       Object1 object1;
-//       Object2 object2;
-//       object1 =new Object1(5, "jose");
-//       
-//       object2 =new Object2(6, "joseMaria");
-//        System.out.println(object1.getName());
+      Obj ob = new obj();
+//        int id = obj.getDept().getSubDept().getHOD().getid();
+       Object1 object1;
+       Object2 object2;
+       object1 =new Object1(5, "jose");
+       
+       object2 =new Object2(6, "joseMaria");
+        System.out.println(object1.getName());
     }
     
 }
