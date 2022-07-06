c1def3b4d3156573997c7043d66e97384e2ce822
jrimbaquingaguana <ricardoimbaquinga@gmail.com>
2022-07-05 19:15:25 -0500

    WS22UnitTest_Area

---
+++ workshops/imbaquingaj/Imbaquinga/Unit2/WS22UnitTest_CalculateAreaOfTriangle/Areas/src/ec/edu/espe/areas/view/Areas.java
@@ -0,0 +1,30 @@
+package ec.edu.espe.areas.view;
+import ec.edu.espe.areas.model.Triangle;
+
+/**
+ *
+ * @author Jose Imbaquinga, DCCO- ESPE, Syntax Error
+ */
+public class Areas {
+
+
+    public static void main(String[] args) {
+        
+        Triangle triangle;
+        double area;
+        
+        triangle = new Triangle(4, 4, 3);
+        area= triangle.CalculateArea();
+        System.out.println(area);
+        triangle = new Triangle(5, 5, 5);
+        area= triangle.CalculateArea();
+        System.out.println(area);        
+        triangle = new Triangle(4.3, 3.4, 5.2);
+        area= triangle.CalculateArea();
+        System.out.println(area);
+        
+        
+   }
+    
+    
+}
