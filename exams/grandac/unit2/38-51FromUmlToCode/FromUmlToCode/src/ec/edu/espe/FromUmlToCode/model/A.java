ed60368b3d32ccf4563a8f9c962552c90a4b5d0e
CarlosJavierGrandaVelasquez <cjgranda1@espe.edu.ec>
2022-07-20 12:13:47 -0500

    ExamUnit2

---
+++ exams/grandac/unit2/38-51FromUmlToCode/FromUmlToCode/src/ec/edu/espe/FromUmlToCode/model/A.java
@@ -0,0 +1,78 @@
+package ec.edu.espe.FromUmlToCode.model;
+
+/**
+ *
+ * @author Carlos Granda, DCCO-ESPE, Syntax Error
+ */
+public class A {
+    private String B;
+    private String C;
+    private String D;
+    private String E;
+
+    public A(String B, String C, String D, String E) {
+        this.B = B;
+        this.C = C;
+        this.D = D;
+        this.E = E;
+    }
+
+    
+    /**
+     * @return the B
+     */
+    public String getB() {
+        return B;
+    }
+
+    /**
+     * @param B the B to set
+     */
+    public void setB(String B) {
+        this.B = B;
+    }
+
+    /**
+     * @return the C
+     */
+    public String getC() {
+        return C;
+    }
+
+    /**
+     * @param C the C to set
+     */
+    public void setC(String C) {
+        this.C = C;
+    }
+
+    /**
+     * @return the D
+     */
+    public String getD() {
+        return D;
+    }
+
+    /**
+     * @param D the D to set
+     */
+    public void setD(String D) {
+        this.D = D;
+    }
+
+    /**
+     * @return the E
+     */
+    public String getE() {
+        return E;
+    }
+
+    /**
+     * @param E the E to set
+     */
+    public void setE(String E) {
+        this.E = E;
+    }
+    
+    
+}
