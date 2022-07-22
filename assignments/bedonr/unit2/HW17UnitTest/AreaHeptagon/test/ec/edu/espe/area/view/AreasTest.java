988313b445e8ef4e75e9e891a8e90d34134b73a7
rabedon1 <alexanderbedon58724222@gmail.com>
2022-07-22 16:54:25 -0500

    Assignments

---
+++ assignments/bedonr/unit2/HW17UnitTest/AreaHeptagon/test/ec/edu/espe/area/view/AreasTest.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
+ */
+package ec.edu.espe.area.view;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author Roberto Bedon, DCCO-ESPE, BettaCoders
+ */
+public class AreasTest {
+    
+    public AreasTest() {
+    }
+
+    /**
+     * Test of main method, of class Areas.
+     */
+    @Test
+    public void testMain() {
+        System.out.println("main");
+        String[] args = null;
+        Areas.main(args);
+       
+    }
+    
+}
