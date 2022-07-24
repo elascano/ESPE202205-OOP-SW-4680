a8f5dab80d2b4da132914c1b2255333a81fc15b5
CarlosJavierGrandaVelasquez <cjgranda1@espe.edu.ec>
2022-07-24 11:11:53 -0500

    WS28GUIAndInterfaces

---
+++ workshops/grandac/unit2/WS28GuiAndInterfeces/FruitAppGui/src/ec/edu/espe/fruitapp/model/Customer.java
@@ -0,0 +1,58 @@
+package ec.edu.espe.fruitapp.model;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+import java.util.TreeSet;
+
+/**
+ *
+ * @author Carlos Granda,DCCO-ESPE, DEES Syntaxi Error
+ */
+public class Customer {
+    private int id;
+    private String name;
+    private float totalSale;
+
+    public Customer(int id, String name, float totalSale) {
+        this.id = id;
+        this.name = name;
+        this.totalSale = totalSale;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    public void see(){
+       List<Customer> customers;
+       customers= new ArrayList<>();
+       
+       Set<String>names;
+       names=new TreeSet<>();
+       
+       
+        
+    }
+    public float getTotalSale() {
+        return totalSale;
+    }
+
+    public void setTotalSale(float totalSale) {
+        this.totalSale = totalSale;
+    }
+    
+            
+}
