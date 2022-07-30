47c87445edcd88e3942e97cf39b317f7c869a097
daponce4 <diegoponce198@gmail.com>
2022-07-30 17:37:26 -0500

    Workshops

---
+++ assignments/ponced/unit2/HW12CRUDPersistence/DataPersistence/DataPersistence/src/utils/Persistence.java
@@ -0,0 +1,10 @@
+
+package utils;
+
+/**
+ *
+ * @author Diego Ponce, DCCO-ESPE, DEES Developers
+ */
+public abstract class Persistence {
+    public abstract void insert();
+}
---
+++ assignments/ponced/unit2/HW19FigureUnitTest/HW18/src/edu/espe/ec/area/view/Area.java
@@ -0,0 +1,18 @@
+package edu.espe.ec.area.view;
+
+import edu.espe.ec.area.model.Parallelogram;
+
+/**
+ *
+ * @author Diego Ponce, DCCO-ESPE, DEES Developers
+ */
+public class Area {
+
+    public static void main(String[] args) {
+        
+        
+       
+        
+    }
+    
+}
