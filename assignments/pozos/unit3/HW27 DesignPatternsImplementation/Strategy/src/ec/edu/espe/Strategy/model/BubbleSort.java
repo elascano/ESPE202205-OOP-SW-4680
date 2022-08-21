1aca0567c92e3cf6f946724d90f7ab898ad3a8c6
stevenpozo <sjpozo1@espe.edu.ec>
2022-08-20 19:39:52 -0500

    homework

---
+++ assignments/pozos/unit3/HW27 DesignPatternsImplementation/Strategy/src/ec/edu/espe/Strategy/model/BubbleSort.java
@@ -0,0 +1,16 @@
+
+package ec.edu.espe.Strategy.model;
+
+/**
+ *
+ * @author Steven Pozo, DCCO-ESPE, DEES Developers
+ */
+public class BubbleSort implements SortingStrategy {
+
+    @Override
+    public int[] sort(int[] data) {
+        System.out.println("que mas pues"); 
+        return null;
+    }
+    
+}
---
+++ assignments/pozos/unit3/HW27 DesignPatternsImplementation/Strategy/src/ec/edu/espe/Strategy/model/SortingStrategy.java
@@ -0,0 +1,10 @@
+
+package ec.edu.espe.Strategy.model;
+
+/**
+ *
+ * @author Steven Pozo, DCCO-ESPE, DEES Developers
+ */
+public interface SortingStrategy {
+    public int[ ] sort( int data[ ] );
+}
