adc34bb9ea0c945e07610c615fa1878e2f830339
unknown <csrivera3@espe.edu.ec>
2022-08-20 20:53:29 -0500

    HW26

---
+++ assignments/riverac/unit3/HW27 DesignPatternsImplementation/FactoryMethod/src/ec/edu/espe/FactoryMethod/model/Connection.java
@@ -0,0 +1,14 @@
+
+package ec.edu.espe.FactoryMethod.model;
+
+/**
+ *
+ * @author Carlos Rivera, DCCO-ESPE, CODEX++
+ */
+public abstract class Connection {
+    public Connection() {
+    }
+    public String description() {
+        return "Generic";
+    }
+}
