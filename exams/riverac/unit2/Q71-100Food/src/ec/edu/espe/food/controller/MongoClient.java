b62a360954f589a72f97871922cdd2a95c023369
unknown <csrivera3@espe.edu.ec>
2022-08-25 08:03:58 -0500

    Exam3P

---
+++ exams/riverac/unit2/Q71-100Food/src/ec/edu/espe/food/controller/MongoClient.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ec.edu.espe.food.controller;
+
+/**
+ *
+ * @author Carlos Rivera, DCCO-ESPE, CODEX++
+ */
+class MongoClient {
+
+    MongoClient(MongoClientURI clientURI) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    MongoDatabase getDatabase(String food) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
---
+++ exams/riverac/unit2/Q71-100Food/src/ec/edu/espe/food/controller/MongoCollection.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ec.edu.espe.food.controller;
+
+import javax.swing.text.Document;
+
+/**
+ *
+ * @author Carlos Rivera, DCCO-ESPE, CODEX++
+ */
+class MongoCollection {
+
+    void insertOne(Document document) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
