987d3338b85bebe148b323e3eed5929ef628240c
jrimbaquingaguana <ricardoimbaquinga@gmail.com>
2022-08-25 00:53:03 -0500

    WS

---
+++ assignments/imbaquingaj/Imbaquinga/Unit3/HW27DesignPattern/FactoryMethod/src/ec/edu/espe/FactoryMethod/model/MySqlConnection.java
@@ -0,0 +1,15 @@
+
+package ec.edu.espe.FactoryMethod.model;
+
+/**
+ *
+ * @author Jose Imbaquinga,DCCO-ESPE, DEES Syntaxi Error
+ */
+public class MySqlConnection extends Connection {
+    public MySqlConnection() {
+    }
+    public String description() {
+        return "MySQL";
+    }
+    
+}
---
+++ assignments/imbaquingaj/Imbaquinga/Unit3/HW27DesignPattern/Observed/src/ec/edu/espe/Observer/model/IInvestion.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package ec.edu.espe.Observer.model;
+
+/**
+ *
+ * @author Steven Pozo, DCCO-ESPE, DEES Developers
+ */
+public interface IInvestion {
+    
+}
